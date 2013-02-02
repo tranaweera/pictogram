730bf4c9d8c86344e38cb3ecc7514e28ccffbab3
tranaweera@gmail.com <tranaweera@gmail.com@34c27ef2-cd98-d818-f4c6-dcc428eb716f>
2013-02-02 05:06:06 +0000

    Test and add the verticle pictogram

---
+++ pictogram/src/main/java/com/thilina/pictogram/Pictogram.java
@@ -0,0 +1,7 @@
+package com.thilina.pictogram;
+
+import java.awt.Graphics2D;
+
+public interface Pictogram {
+	void draw(Graphics2D graphics);
+}
--- pictogram/src/main/java/com/thilina/pictogram/VerticlePictogram.java
+++ pictogram/src/main/java/com/thilina/pictogram/VerticlePictogram.java
@@ -6,45 +6,48 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.Serializable;
 
-public class HorizontalPictogram implements Serializable {
+public class VerticlePictogram implements Pictogram, Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private int scale;
 	private BufferedImage imageOne;
 	private BufferedImage imageTwo;
 	private int imageScale;
-	private int maxPercentage;
+	private int percentage;
 	
-	public HorizontalPictogram(int scale, BufferedImage imageOne,
-			BufferedImage imageTwo, int imageScale, int maxPercentage) {
-		super();
+	public VerticlePictogram(int scale, BufferedImage imageOne,
+			BufferedImage imageTwo, int imageScale, int percentage) {
 		this.scale = scale;
 		this.imageOne = imageOne;
 		this.imageTwo = imageTwo;
 		this.imageScale = imageScale;
-		this.maxPercentage = maxPercentage;
+		this.percentage = percentage;
 	}
+
 	
-	public void draw(Graphics2D graphics){
+	public void draw(Graphics2D graphics) {
+		
 		graphics.setStroke(new BasicStroke(5));
 		graphics.setBackground(Color.WHITE);
 		graphics.fillRect(0, 0, scale, scale);
+		
 
 		int count =1;		
-		for (int g = 1; g < scale; g = g + imageScale) {
-			for (int i = 1; i < scale; i = i + imageScale) {
-
-				if (count <= (100-maxPercentage)) {
-					graphics.drawImage(imageOne, i, g, null);
+		for (int g = (scale-imageScale); g >= 0; g = g - imageScale) {
+			for (int i = (scale-imageScale); i >= 0; i = i - imageScale) {
+				if (count <= percentage) {
+					graphics.drawImage(imageTwo, g, i, null);
 				} else {
-					graphics.drawImage(imageTwo, i, g, null);
+					graphics.drawImage(imageOne, g, i, null);
 				}
 				++count;
 			}
 		}
-	}	
+
+	}
+
 }
